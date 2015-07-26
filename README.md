0fac9cd569e032d50dbdd344e20024f0041d40eb
crossjs <liwenfu@crossjs.com>
2015-07-26 17:55:01 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # nd-gallery
 
-[![spm version](http://spmjs.io/badge/nd-gallery)](http://spmjs.io/package/nd-gallery)
+[![spm version](http://spm.crossjs.com/badge/nd-gallery)](http://spm.crossjs.com/package/nd-gallery)
 
 > 多图幻灯片浏览
 
@@ -15,34 +15,4 @@ $ spm install nd-gallery --save
 ```js
 var Gallery = require('nd-gallery');
 // use Gallery
-```
-## 开发
-
-### 本地 Web 服务
-
-```bash
-grunt
-```
-
-浏览器中访问 http://127.0.0.1:8851
-
-### 生成/查看 API 文档
-
-```bash
-grunt doc
-grunt
-```
-
-浏览器中访问 http://127.0.0.1:8851/doc
-
-### 代码检查与单元测试
-
-```bash
-grunt test
-```
-
-### 发布组件到 SPM 源
-
-```bash
-grunt publish
 ```
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /**
- * @module: nd-gallery
- * @author: crossjs <liwenfu@crossjs.com> - 2015-01-22 13:36:51
+ * @module Gallery
+ * @author crossjs <liwenfu@crossjs.com>
  */
 
 'use strict';
 
-var $ = require('jquery'),
-  Overlay = require('nd-overlay'),
-  Template = require('nd-template'),
-  ndImage = require('nd-image');
+var $ = require('jquery');
+var Overlay = require('nd-overlay');
+var Template = require('nd-template');
+// var ndImage = require('nd-image');
 
-var Gallery = module.exports = Overlay.extend({
+var Gallery = Overlay.extend({
 
-  Implements: Template,
+  Implements: [Template],
 
   attrs: {
     template: require('./src/gallery.handlebars'),
 
     align: null,
+
     position: 'fixed',
-    width: 1024,
 
     classPrefix: 'ui-gallery',
     index: 0,
@@ -27,43 +27,20 @@ var Gallery = module.exports = Overlay.extend({
 
     events: {
       'keyup': 'close',
-      'click [data-role=close]': 'close',
-      'click [data-role=prev]': 'prev',
-      'click [data-role=next]': 'next',
-      'click [data-role=thumb]': 'switchTo'
-    },
-
-    afterRender: '_resizeThumbs',
-    afterShow: '_focus',
-
-    beforeShow: '_beforeShow',
-    afterHide: '_afterHide'
+      'click [data-role="close"]': 'close',
+      'click [data-role="prev"]': 'prev',
+      'click [data-role="next"]': 'next',
+      'click [data-role="thumb"]': 'switchTo'
+    }
   },
 
-  parseElement: function() {
+  initAttrs: function(config) {
+    Gallery.superclass.initAttrs.call(this, config);
+
     this.set('model', {
-      classPrefix: this.get('classPrefix'),
       index: this.get('index'),
       items: this.get('items')
     });
-
-    Gallery.superclass.parseElement.call(this);
-  },
-
-  setup: function() {
-    var timeout, that = this;
-
-    $(window).on('resize', function() {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-
-      timeout = setTimeout(function() {
-        that._beforeShow();
-      }, 80);
-    });
-
-    // Gallery.superclass.setup.call(this);
   },
 
   close: function(e) {
@@ -96,173 +73,27 @@ var Gallery = module.exports = Overlay.extend({
     this.set('index', num);
   },
 
-  _resizeThumbs: function() {
-    this.$('[data-role=thumb]').each(function(i, img) {
-      ndImage.load({
-        node: img,
-        // url: img.src,
-
-        ready: function() {
-          // 缩放
-          ndImage.zoom({
-            node: img,
-            width: this.width,
-            height: this.height,
-            maxWidth: 60,
-            maxHeight: 60,
-            overflow: true,
-
-            callback: function(w, h) {
-              this.width = w;
-              this.height = h;
-            }
-          });
-
-          // 居中
-          ndImage.center({
-            node: img,
-            width: 60,
-            height: 60,
-
-            callback: function(t, l) {
-              $(this).css({
-                marginTop: t,
-                marginLeft: l
-              });
-            }
-          });
-        }
-      });
-    });
-  },
-
-  _focus: function() {
-    this.element[0].focus();
-  },
-
-  _afterHide: function() {
-    document.documentElement.style.overflow = '';
-  },
-
-  _beforeShow: function() {
-    document.documentElement.style.overflow = 'hidden';
-    this.set('height', Math.max(400, Math.min(768, $(window).height())));
-  },
-
-  _onRenderWidth: function(width) {
-    this.$('[data-role=content]').width(width);
-  },
-
-  _onRenderHeight: function(height) {
-    this.$('[data-role=content]').css({
-      top: Math.max(0, ($(window).height() - height) / 2),
-      height: height
-    });
-
-    this.$('[data-role=image-wrap]').height(height - 90);
-
-    this._onRenderIndex(this.get('index'));
-  },
-
   _onRenderIndex: function(index) {
-    var that = this,
-      width = this.get('width'),
-      height = this.get('height'),
-      items = this.get('items'),
-      img = this.$('[data-role=image]')[0];
+    var items = this.get('items'),
+      img = this.$('[data-role="image"]')[0];
 
-    if (!width || !height) {
-      return setTimeout(function() {
-        that._onRenderIndex(index);
-      }, 80);
-    }
+    // 原图
+    this.$('[data-role="origin"]')[0].href = items[index].large;
 
-    height -= 90;
-
-    // 原图/大图
-    this.$('[data-role=origin]')[0].href = items[index].large;
-
-    // 展示区域先加载小图
-    img.src = items[index].small;
-
-    // 缩放
-    ndImage.zoom({
-      node: img,
-      maxWidth: width,
-      maxHeight: height,
-      fixed: true,
-
-      callback: function(w, h) {
-        this.width = w;
-        this.height = h;
-      }
-    });
-
-    // 居中
-    ndImage.center({
-      node: img,
-      width: width,
-      height: height,
-
-      callback: function(t, l) {
-        $(this).css({
-          marginTop: t,
-          marginLeft: l
-        });
-      }
-    });
-
-    // 加载中图
-    (function(index) {
-      ndImage.load({
-        url: items[index].medium,
-
-        ready: function() {
-          // 缩放
-          ndImage.zoom({
-            node: img,
-            width: this.width,
-            height: this.height,
-            maxWidth: width,
-            maxHeight: height,
-            fixed: true,
-
-            callback: function(w, h) {
-              this.width = w;
-              this.height = h;
-            }
-          });
-
-          // 居中
-          ndImage.center({
-            node: img,
-            width: width,
-            height: height,
-
-            callback: function(t, l) {
-              $(this).css({
-                marginTop: t,
-                marginLeft: l
-              });
-            }
-          });
-        },
-
-        load: function() {
-          if (index === that.get('index')) {
-            img.src = items[index].medium;
-          }
-        }
-      });
-    })(index);
+    // 大图
+    img.src = items[index].medium;
 
     // thumbs
-    this.$('[data-role=thumbs]')
-      .children(':eq(' + index + ')').addClass('current')
-      .siblings('.current').removeClass('current');
+    this.$('[data-role="thumbs"]')
+      .children(':eq(' + index + ')')
+      .addClass('current')
+      .siblings('.current').removeClass('current')
+      .end().get(0).scrollIntoViewIfNeeded();
 
     // pagination
-    this.$('[data-role=index]').text(index + 1);
+    this.$('[data-role="index"]').text(index + 1);
   }
 
 });
+
+module.exports = Gallery;
