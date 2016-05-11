e88aae7af39d5b6903ab881078937d9172bc0982
crossjs <liwenfu@crossjs.com>
2016-05-11 15:16:27 +0800

    update UT; update ESLINT

--- README.md
+++ README.md
@@ -3,18 +3,49 @@
 [![Travis](https://img.shields.io/travis/ndfront/nd-gallery.svg?style=flat-square)](https://github.com/ndfront/nd-gallery)
 [![Coveralls](https://img.shields.io/coveralls/ndfront/nd-gallery.svg?style=flat-square)](https://github.com/ndfront/nd-gallery)
 [![NPM version](https://img.shields.io/npm/v/nd-gallery.svg?style=flat-square)](https://npmjs.org/package/nd-gallery)
+[![dependencies](https://david-dm.org/ndfront/nd-gallery.svg?style=flat-square)](https://david-dm.org/ndfront/nd-gallery)
+[![devDependency Status](https://david-dm.org/ndfront/nd-gallery/dev-status.svg?style=flat-square)](https://david-dm.org/ndfront/nd-gallery#info=devDependencies)
 
 > 多图幻灯片浏览
 
 ## 安装
 
-```bash
+``` bash
 $ npm install nd-gallery --save
+```
+
+## 开发
+
+``` bash
+# 代码静态检查
+$ npm run lint
+
+# 代码静态检查并自动修复
+$ npm run lint:fix
+
+# 运行单元测试
+$ npm run test
+
+# 热重载模式下运行单元测试
+$ npm run test:dev
 ```
 
 ## 使用
 
-```js
+``` js
 var Gallery = require('nd-gallery');
 // use Gallery
+new Gallery({
+  items: [{
+    large: '<URL>',
+    medium: '<URL>',
+    small: '<URL>',
+    title: '<TITLE>'
+  }, {
+    large: '<URL>',
+    medium: '<URL>',
+    small: '<URL>',
+    title: '<TITLE>'
+  }]
+}).render()
 ```
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,16 +1,45 @@
 'use strict'
 
-// var $ = require('nd-jquery')
-var chai = require('chai')
-var sinonChai = require('sinon-chai')
-var Gallery = require('../index')
 var $ = require('nd-jquery')
+var chai = require('chai')
+// var sinonChai = require('sinon-chai')
+var Gallery = require('../index')
 var expect = chai.expect
 // var sinon = window.sinon
 
-chai.use(sinonChai)
+// chai.use(sinonChai)
 
-/*globals describe,it,after*/
+/* globals describe,it,afterEach */
+
+var BLANK_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+
+function createGallery(type) {
+  if (type === 0) {
+    return new Gallery({
+      zIndex: 2,
+      items: []
+    }).render()
+  } else {
+    return new Gallery({
+      zIndex: 2,
+      index: type === 1 ? 3 : 0,
+      items: [{
+        id: 'c6bdfb54-d746-4463-8e5b-1ba5761119b5',
+        large: BLANK_IMAGE,
+        medium: BLANK_IMAGE,
+        small: BLANK_IMAGE,
+        title: '2016-03-23 16:00:39'
+      }, {
+        id: '5fbcd401-8764-4564-957c-289b5236acd1',
+        large: BLANK_IMAGE,
+        medium: BLANK_IMAGE,
+        small: BLANK_IMAGE,
+        title: '2016-03-23 16:00:38'
+      }]
+    }).render()
+  }
+}
+
 describe('Gallery', function() {
   it('new Gallery', function() {
     expect(Gallery).to.be.a('function')
@@ -18,106 +47,56 @@ describe('Gallery', function() {
   })
 })
 
-var gallery
-var items
-function getCurIndex() {
-  return $('.thumbs-wrap .thumbs').find('.current').index()
-}
-
-function renderByType(type) {
-  if (type === 0) {
-    items = []
-    gallery = new Gallery({
-      zIndex: 2,
-      items: items
-    }).render()
-  } else if (type === 1) {
-    items = [{
-      id: 'c6bdfb54-d746-4463-8e5b-1ba5761119b5',
-      large: 'http://betacs.101.com/v0.1/download?dentryId=c6bdfb54-d746-4463-8e5b-1ba5761119b5',
-      medium: 'http://betacs.101.com/v0.1/download?dentryId=c6bdfb54-d746-4463-8e5b-1ba5761119b5&size=640',
-      small: 'http://betacs.101.com/v0.1/download?dentryId=c6bdfb54-d746-4463-8e5b-1ba5761119b5&size=80',
-      title: '2016-03-23 16:00:39',
-    }, {
-      id: '5fbcd401-8764-4564-957c-289b5236acd1',
-      large: 'http://betacs.101.com/v0.1/download?dentryId=5fbcd401-8764-4564-957c-289b5236acd1',
-      medium: 'http://betacs.101.com/v0.1/download?dentryId=5fbcd401-8764-4564-957c-289b5236acd1&size=640',
-      small: 'http://betacs.101.com/v0.1/download?dentryId=5fbcd401-8764-4564-957c-289b5236acd1&size=80',
-      title: '2016-03-23 16:00:38',
-    }]
-    gallery = new Gallery({
-      zIndex: 2,
-      index: 3,
-      items: items
-    }).render()
-  } else if (type === 2) {
-    items = [{
-      id: 'c6bdfb54-d746-4463-8e5b-1ba5761119b5',
-      large: 'http://betacs.101.com/v0.1/download?dentryId=c6bdfb54-d746-4463-8e5b-1ba5761119b5',
-      medium: 'http://betacs.101.com/v0.1/download?dentryId=c6bdfb54-d746-4463-8e5b-1ba5761119b5&size=640',
-      small: 'http://betacs.101.com/v0.1/download?dentryId=c6bdfb54-d746-4463-8e5b-1ba5761119b5&size=80',
-      title: '2016-03-23 16:00:39',
-    }, {
-      id: '5fbcd401-8764-4564-957c-289b5236acd1',
-      large: 'http://betacs.101.com/v0.1/download?dentryId=5fbcd401-8764-4564-957c-289b5236acd1',
-      medium: 'http://betacs.101.com/v0.1/download?dentryId=5fbcd401-8764-4564-957c-289b5236acd1&size=640',
-      small: 'http://betacs.101.com/v0.1/download?dentryId=5fbcd401-8764-4564-957c-289b5236acd1&size=80',
-      title: '2016-03-23 16:00:38',
-    }]
-    gallery = new Gallery({
-      zIndex: 2,
-      items: items
-    }).render()
-  }
-}
-
 describe('show/hide', function() {
-  after(function() {
+  var gallery
+
+  afterEach(function() {
     gallery.destroy()
   })
-  it('show，显示gallery close方法,隐藏gallery', function() {
-    //传入空值 显示
-    renderByType(0)
+
+  it('默认 index', function() {
+    gallery = createGallery(0)
     $(gallery).trigger('show')
     expect($('.ui-gallery').css('display')).to.equal('block')
-    gallery.destroy()
+  })
 
-    //传入非法index 显示
-    renderByType(1)
+  it('非法 index', function() {
+    gallery = createGallery(1)
     $(gallery).trigger('show')
     expect($('.ui-gallery').css('display')).to.equal('block')
-    gallery.destroy()
+  })
 
-    //传入正常值
-    renderByType(2)
+  it('正常 index', function() {
+    gallery = createGallery(2)
     $(gallery).trigger('show')
     expect($('.ui-gallery').css('display')).to.equal('block')
 
-    //点击关闭-close
-    var e = {}
-    gallery.close(e)
+    // 点击关闭-close
+    gallery.close({})
     expect($('.ui-gallery').css('display')).to.equal('none')
 
-    //按下ESC-close
     $(gallery).trigger('show')
-    e.keyCode = 26
-    gallery.close(e)
+    // fake some key
+    gallery.close({
+      keyCode: 26
+    })
     expect($('.ui-gallery').css('display')).to.equal('block')
 
-    e.keyCode = 27
-    gallery.close(e)
+    // fake esc
+    gallery.close({
+      keyCode: 27
+    })
     expect($('.ui-gallery').css('display')).to.equal('none')
-
   })
 })
 
 describe('next/prev', function() {
-  after(function() {
-    gallery.destroy()
-  })
   it('选择上一张下一张某一张正确跳转', function() {
-    //两张图片
-    renderByType(2)
+    function getCurIndex() {
+      return $('.thumbs-wrap .thumbs').find('.current').index()
+    }
+    // 两张图片
+    var gallery = createGallery(2)
     var length = $('.thumbs-wrap .thumbs li').length
     var curIndex = getCurIndex()
     gallery.prev()
@@ -141,7 +120,6 @@ describe('next/prev', function() {
       $('[data-role="thumb"][data-index="' + i + '"]').trigger('click')
       expect(getCurIndex()).to.equal(i)
     }
-
-
+    gallery.destroy()
   })
 })
